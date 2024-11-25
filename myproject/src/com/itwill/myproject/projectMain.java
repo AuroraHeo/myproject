57443b53a2e59f6ad6967c3320cb2234fac2fb89
AuroraHeo <youjin2586@gmail.com>
2024-11-25 14:32:33 +0900

    최종본

--- myproject/src/com/itwill/myproject/projectMain.java
+++ myproject/src/com/itwill/myproject/projectMain.java
@@ -7,8 +7,9 @@ public class projectMain {
 		System.out.println("안녕하세요, 깃허브!");
 		System.out.println("이 줄은 깃허브에서 작성해봄...");
 		System.out.println("gkgk");
-		System.out.println("르뀨 glglglgl");
+		System.out.println("르뀨 내가 먼저할랭");
 		System.out.println("르뀨2");
+		System.out.println("수정함");
 	}
 
 }
